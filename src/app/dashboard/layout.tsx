bad807483eed1b378385f8343f8b2b4e5228f00d
Gabriel Griffin Silva <griffinsilva10@gmail.com>
2024-06-11 14:40:14 -0600

    card-carousel-checkbox

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -14,6 +14,9 @@ const links = [
   { name: "badge", href: "badge" },
   { name: "calendar", href: "calendar" },
   { name: "avatar", href: "avatar" },
+  { name: "card", href: "card" },
+  { name: "carousel", href: "carousel" },
+  { name: "checkbox", href: "checkbox" },
 ].sort((a, b) => a.name.localeCompare(b.name));
 
 export default function DashboardLayout({
